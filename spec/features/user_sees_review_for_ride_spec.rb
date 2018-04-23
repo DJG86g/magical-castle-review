63fa08c6ab3d8d2c64b27f13f8e5ed729293d369
Daniel Gouveia <danshome@Daniels-MacBook-Pro.local>
2018-04-23 15:23:20 -0400

    added new tests

---
+++ spec/features/user_sees_review_for_ride_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature "user sees list of reviews on ride page" do
+  scenario "sees reviews for specific restaurant" do
+    space_mountain = Ride.create(name: 'Space mountain')
+    review_for_space = Review.create(rating: 4, body: "This mountain is amazing!!", ride: space_mountain)
+
+    visit restaurant_path(space_mountain)
+
+    expect(page).to have_content space_mountain.name
+    save_and_open_page
+    expect(page).to have_content review_for_space.rating
+    expect(page).to have_content review_for_space.body
+    expect(page).to have_content review_for_space.created_at.strftime("%B %d %Y")
+  end
+
+  scenario "does not see other reviews for other rides" do
+    space_mountain = Ride.create(name: 'Space mountain')
+    thunder_mountain = Ride.create(name: "Thunder mountain")
+    review_for_space = Review.create(rating: 4, body: "This mountain is amazing!!", ride: space_mountain)
+    review_for_thunder = Review.create(rating: 5, body: "so good", ride: thunder_mountain)
+
+    visit restaurant_path(thunder_mountain)
+
+    expect(page).to have_content thunder_mountain.name
+    expect(page).to have_content review_for_thunder.body
+
+    expect(page).not_to have_content space_mountain.name
+    expect(page).not_to have_content review_for_space.rating
+    expect(page).not_to have_content review_for_space.body
+  end
+end
