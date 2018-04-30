166444d0173a7259751aef1bf2dbbbf528283d6b
Eric Schaeffer <eschaeffer108@gmail.com>
2018-04-30 13:57:58 -0400

    Added_the_reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-       
+
+  validates :username, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :date_of_birth, presence: true 
+  has_many :reviews
+  has_many :rides, through: :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_26_192952) do
+ActiveRecord::Schema.define(version: 2018_04_27_202057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2018_04_26_192952) do
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "body", null: false
+    t.integer "rating", null: false
+    t.bigint "user_id"
+    t.bigint "ride_id"
+    t.index ["ride_id"], name: "index_reviews_on_ride_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "rides", force: :cascade do |t|
