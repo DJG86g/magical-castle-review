b2b6347a0c9ee866d4f6cf0fe818282d012a2fa4
David Garcia <davidgarcia@Davids-MacBook-Pro.local>
2018-05-04 14:28:07 -0400

    react_update

--- spec/features/02_visitor_sees_reviews_for_ride_spec.rb
+++ spec/features/02_visitor_sees_reviews_for_ride_spec.rb
@@ -8,15 +8,16 @@ require 'rails_helper'
 
 feature "visitor sees list of reviews on ride page" do
   scenario "sees reviews for specific ride" do
-    thunder_mountain = Ride.create(name: 'Thunder Mountain', description: 'Race through a haunted gold-mining town aboard a rollicking runaway mine train.')
-    review_for_thunder_mountain = Review.create(rating: 4, body: "This ride was my favorite part of the whole vacation!  I rode it three times in a row!", ride: thunder_mountain)
+    park = Park.create(name: "Magic Kingdom",address: "1180 Seven Seas Dr",city: "Lake Buena Vista",state: "FL",zip: 32830 ,description: "The park is represented by Cinderella Castle, inspired by the fairy tale castle seen in the 1950 film.")
+    ride = Ride.create(name: 'Thunder Mountain', description: 'Race through a haunted gold-mining town aboard a rollicking runaway mine train.', park: park)
+    review = Review.create(rating: 4, body: "This ride was my favorite part of the whole vacation!  I rode it three times in a row!", ride: ride)
 
-    visit ride_path(thunder_mountain)
-
-    expect(page).to have_content thunder_mountain.name
-    expect(page).to have_content review_for_thunder_mountain.rating
-    expect(page).to have_content review_for_thunder_mountain.body
-    expect(page).to have_content review_for_thunder_mountain.created_at.strftime("%B %d %Y")
+    visit park_ride_path(park, ride)
+    save_and_open_page
+    expect(page).to have_content ride.name
+    expect(page).to have_content review.rating
+    expect(page).to have_content review.body
+    expect(page).to have_content review.created_at.strftime("%B %d %Y")
   end
 
   scenario "does not see other reviews for other restaurants" do
