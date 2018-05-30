c7591286bd8bd717ffc7adf4ce08841f77477e92
Daniel Gouveia <danshome@Daniels-MacBook-Pro-2.local>
2018-05-30 16:50:34 -0400

    changed_font_all_tests_passing

--- spec/features/profile_photo_spec.rb
+++ spec/features/profile_photo_spec.rb
@@ -4,7 +4,7 @@ feature "profile photo" do
   scenario "user uploads a profile photo" do
     visit root_path
     click_link "Sign Up"
-
+    save_and_open_page
     fill_in 'username', with: 'Mickey1955'
     fill_in 'First name', with: 'Walt'
     fill_in 'Last name', with: 'Disney'
